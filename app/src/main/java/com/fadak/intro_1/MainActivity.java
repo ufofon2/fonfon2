dda3ee0eba3aeca6926a944a031c6b003161c04c
SongMinwoo <asd0828@gmail.com>
2016-09-02 11:53:57 +0900

    aa

--- app/src/main/java/com/fadak/intro_1/MainActivity.java
+++ app/src/main/java/com/fadak/intro_1/MainActivity.java
@@ -1,7 +1,11 @@
 package com.fadak.intro_1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent =
+                        new Intent(MainActivity.this,NextActivity.class);
+                startActivity(intent);
+            }
+        };
+        Timer timer = new Timer();
+        timer.schedule(task,3000);
     }
 }
---
+++ app/src/main/java/com/fadak/intro_1/NextActivity.java
@@ -0,0 +1,13 @@
+package com.fadak.intro_1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NextActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.next);
+    }
+}
